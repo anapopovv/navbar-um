41ec6cf73ae76ece8cc7afc98154b19558c105a5
Ana Popov <99069162+anapopovv@users.noreply.github.com>
2024-05-08 17:22:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const toggleModal = () => document.body.classList.toggle("open");
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
